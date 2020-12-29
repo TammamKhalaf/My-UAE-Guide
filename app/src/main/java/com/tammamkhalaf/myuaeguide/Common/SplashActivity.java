91b44bcb7645d230b6b1e66b77316bfe61c37e6d
Tammam Khalaf <tammam-963@hotmail.com>
2020-12-30 00:26:39 +0400

    add files to splash_screen activity

--- app/src/main/java/com/tammamkhalaf/myuaeguide/Common/SplashActivity.java
+++ app/src/main/java/com/tammamkhalaf/myuaeguide/Common/SplashActivity.java
@@ -4,15 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.tammamkhalaf.myuaeguide.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIMER = 5000;
+
+    ImageView backgroundIV;
+    TextView powered_by_line;
+
+    Animation sideAnim,bottomAnim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash_screen);
+
+
+        backgroundIV.findViewById(R.id.background_image);
+        powered_by_line.findViewById(R.id.powered_by_line);
+
+
+        sideAnim = AnimationUtils.loadAnimation(SplashActivity.this,R.anim.side_anim);
+        bottomAnim = AnimationUtils.loadAnimation(SplashActivity.this,R.anim.bottom_anim);
+
+        backgroundIV.setAnimation(sideAnim);
+        powered_by_line.setAnimation(bottomAnim);
+
     }
+
 }
