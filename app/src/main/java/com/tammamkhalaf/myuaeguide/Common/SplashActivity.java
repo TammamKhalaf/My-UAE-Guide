caa0830c0b07976d265fcfa4e936b2dc8a4902eb
Tammam Khalaf <tammam-963@hotmail.com>
2020-12-29 20:47:07 +0400

    add nice image and logo to the splach screen

--- app/src/main/java/com/tammamkhalaf/myuaeguide/Common/SplashActivity.java
+++ app/src/main/java/com/tammamkhalaf/myuaeguide/Common/SplashActivity.java
@@ -1,14 +1,18 @@
-package com.tammamkhalaf.myuaeguide;
+package com.tammamkhalaf.myuaeguide.Common;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.tammamkhalaf.myuaeguide.R;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash_screen);
     }
 }
