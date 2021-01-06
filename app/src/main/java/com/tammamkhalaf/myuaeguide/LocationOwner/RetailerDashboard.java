b3d7870d2d7459f11cc7bcf2d5e2610640edbb98
Tammam Khalaf <tammam-963@hotmail.com>
2021-01-06 22:48:54 +0400

    complete part 20

--- app/src/main/java/com/tammamkhalaf/myuaeguide/LocationOwner/RetailerDashboard.java
+++ app/src/main/java/com/tammamkhalaf/myuaeguide/LocationOwner/RetailerDashboard.java
@@ -1,10 +1,13 @@
 package com.tammamkhalaf.myuaeguide.LocationOwner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.TextView;
 
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 import com.tammamkhalaf.myuaeguide.Databases.SessionManager;
 import com.tammamkhalaf.myuaeguide.R;
 
@@ -12,22 +15,37 @@ import java.util.HashMap;
 
 public class RetailerDashboard extends AppCompatActivity {
 
+    ChipNavigationBar chipNavigationBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_retailer_dashboard);
 
+        chipNavigationBar = findViewById(R.id.bottom_nav_menu);
+        chipNavigationBar.setItemSelected(R.id.bottom_nav_dashboard,true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new RetailerDashboardFragment()).commit();
+        bottomMenu();
+    }
 
-        TextView textView = findViewById(R.id.textView);
+    private void bottomMenu() {
 
-        SessionManager sessionManager = new SessionManager(this);
-
-        HashMap<String,String> userDetails = sessionManager.getUsersDetailFromSession();
-
-        String fullName = userDetails.get(sessionManager.KEY_FULLNAME);
-        String phoneNo = userDetails.get(sessionManager.KEY_PHONE_NUMBER);
-
-        textView.setText(fullName+"\n"+phoneNo);
+        chipNavigationBar.setOnItemSelectedListener(i -> {
+            Fragment fragment = null;
+            switch (i) {
+                case R.id.bottom_nav_dashboard:
+                    fragment = new RetailerDashboardFragment();
+                    break;
+                case R.id.bottom_nav_manage:
+                    fragment = new RetailerManageFragment();
+                    break;
+                case R.id.bottom_nav_profile:
+                    fragment = new RetailerProfileFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+        });
 
     }
 }
