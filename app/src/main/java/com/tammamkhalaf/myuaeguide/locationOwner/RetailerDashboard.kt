89d2f3f9545b2f946ae63c6f3b85ba2e8d866a11
Tammam Khalaf <tammam-963@hotmail.com>
2021-01-19 20:09:39 +0400

    modifying RetailerDashboard.kt bottom navigation between fragments

--- app/src/main/java/com/tammamkhalaf/myuaeguide/locationOwner/RetailerDashboard.kt
+++ app/src/main/java/com/tammamkhalaf/myuaeguide/locationOwner/RetailerDashboard.kt
@@ -7,30 +7,36 @@ import androidx.fragment.app.Fragment
 import com.ismaeldivita.chipnavigation.ChipNavigationBar
 import com.tammamkhalaf.myuaeguide.R
 
+
 class RetailerDashboard : AppCompatActivity() {
+
     var chipNavigationBar: ChipNavigationBar? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_retailer_dashboard)
         chipNavigationBar = findViewById(R.id.bottom_nav_menu)
-        chipNavigationBar?.run { setItemSelected(R.id.bottom_nav_dashboard, true) }
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, RetailerDashboardFragment()).commit()
+        chipNavigationBar?.setItemSelected(R.id.bottom_nav_profile, true)
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, RetailerProfileFragment()).commit()
         bottomMenu()
     }
 
     private fun bottomMenu() {
-        chipNavigationBar?.setOnClickListener {
-            var fragment: Fragment? = null
-            when (it.id) {
-                R.id.bottom_nav_dashboard -> fragment = RetailerDashboardFragment()
-                R.id.bottom_nav_manage -> fragment = RetailerManageFragment()
-                R.id.bottom_nav_profile -> fragment = RetailerProfileFragment()
+        chipNavigationBar?.setOnItemSelectedListener(object : ChipNavigationBar.OnItemSelectedListener {
+            override fun onItemSelected(i: Int) {
+                var fragment: Fragment? = null
+                when (i) {
+                    R.id.bottom_nav_dashboard -> fragment = RetailerDashboardFragment()
+                    R.id.bottom_nav_manage -> fragment = RetailerManageFragment()
+                    R.id.bottom_nav_profile -> fragment = RetailerProfileFragment()
+                }
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment!!).commit()
             }
-            if (fragment != null) {
-                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
-            }
-        }
+        })
+    }
+
+    companion object {
+        private const val TAG = "RetailerDashboard"
     }
 
 }
