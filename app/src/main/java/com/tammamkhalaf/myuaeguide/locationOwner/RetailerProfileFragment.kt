0394318efe3bf6fad0ae7ff24baaa48b50920362
Tammam Khalaf <tammam-963@hotmail.com>
2021-01-20 15:52:57 +0400

    Query Object read from real time database
    OrderByKey()
    
    OrderByChild()

--- app/src/main/java/com/tammamkhalaf/myuaeguide/locationOwner/RetailerProfileFragment.kt
+++ app/src/main/java/com/tammamkhalaf/myuaeguide/locationOwner/RetailerProfileFragment.kt
@@ -1,8 +1,6 @@
 package com.tammamkhalaf.myuaeguide.locationOwner
 
-import android.content.Intent
 import android.os.Bundle
-import android.se.omapi.Session
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -11,37 +9,40 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.google.android.gms.tasks.OnFailureListener
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import com.tammamkhalaf.myuaeguide.R
 import com.tammamkhalaf.myuaeguide.databases.SessionManager
+import com.tammamkhalaf.myuaeguide.databases.firebase.models.User
 
 /**
  * A simple [Fragment] subclass.
- * Use the [RetailerProfileFragment.newInstance] factory method to
+ * Use the [RetailerProfileFragment.new Instance] factory method to
  * create an instance of this fragment.
  */
 class RetailerProfileFragment : Fragment() {
-
+    private lateinit var usernameEditText:EditText
+    private lateinit var emailEditText:EditText
+    lateinit var fullNameEditText:EditText
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
 
-        var view:View = inflater.inflate(R.layout.fragment_retailer_profile, container, false)
+        val view:View = inflater.inflate(R.layout.fragment_retailer_profile, container, false)
 
 
-        var text:EditText =  view.findViewById(R.id.profile_name)
+         usernameEditText =  view.findViewById(R.id.profile_name)
+         emailEditText =  view.findViewById(R.id.profile_email)
+         fullNameEditText =  view.findViewById(R.id.profile_fullname)
 
-        var btn: Button = view.findViewById(R.id.profile_button_update)
+        val btn: Button = view.findViewById(R.id.profile_button_update)
 
         btn.setOnClickListener {
-            if (text.text.isNotEmpty()) {
-                Log.d(TAG, "text.text : ${text.text}")
-
+            if (usernameEditText.text.isNotEmpty()) {
+                Log.d(TAG, "text.text : ${usernameEditText.text}")
                 val reference = FirebaseDatabase.getInstance().getReference("Users")
-
-                reference.child(SessionManager.usersDetailFromSession["phoneNo"]!!).child("username").setValue(text.text.toString())
+                reference.child(SessionManager.usersDetailFromSession["phoneNo"]!!).child("username").setValue(usernameEditText.text.toString())
                         .addOnSuccessListener {
                     Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
                 }.addOnFailureListener {
@@ -49,9 +50,91 @@ class RetailerProfileFragment : Fragment() {
                 }
 
             }
+
+            if (emailEditText.text.isNotEmpty()) {
+                Log.d(TAG, "text.text : ${usernameEditText.text}")
+                val reference = FirebaseDatabase.getInstance().getReference("Users")
+                reference.child(SessionManager.usersDetailFromSession["phoneNo"]!!).child("email").setValue(emailEditText.text.toString())
+                        .addOnSuccessListener {
+                    Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
+                }.addOnFailureListener {
+                            Log.d(TAG, ": onFailureListener $it")
+                }
+
+            }
+
+            if (fullNameEditText.text.isNotEmpty()) {
+                Log.d(TAG, "text.text : ${usernameEditText.text}")
+                val reference = FirebaseDatabase.getInstance().getReference("Users")
+                reference.child(SessionManager.usersDetailFromSession["phoneNo"]!!).child("fullName").setValue(fullNameEditText.text.toString())
+                        .addOnSuccessListener {
+                    Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
+                }.addOnFailureListener {
+                            Log.d(TAG, ": onFailureListener $it")
+                }
+
+            }
+
         }
 
         return view
+    }
+
+    init {
+        getUserAccountsData()
+    }
+
+    private fun getUserAccountsData(){
+        val reference:DatabaseReference = FirebaseDatabase.getInstance().reference
+        /**
+         *
+         * ----------------- Query 1-----------
+         * */
+
+
+        val query1:Query = reference.child("Users").orderByKey().equalTo(SessionManager.usersDetailFromSession["phoneNo"])
+        
+        query1.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (singleSnapshot in snapshot.children) {
+                    val user: User? = singleSnapshot.getValue(User::class.java)
+                    fullNameEditText.setText(user?.fullName)
+                    usernameEditText.setText(user?.username)
+                    emailEditText.setText(user?.email)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e(TAG, "onCancelled: ", error.message as Throwable)
+            }
+
+        })
+
+        /**
+         *
+         * ----------------- Query 2-----------
+         * */
+
+        val query2:Query = reference.child("Users")
+                .orderByChild("phoneNo")
+                .equalTo(SessionManager.usersDetailFromSession["phoneNo"])
+
+        query2.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (singleSnapshot in snapshot.children) {
+                    val user: User? = singleSnapshot.getValue(User::class.java)
+                    fullNameEditText.setText(user?.fullName)
+                    usernameEditText.setText(user?.username)
+                    emailEditText.setText(user?.email)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e(TAG, "onCancelled: ",error.details as Throwable )
+            }
+
+        })
+
     }
 
     companion object {
